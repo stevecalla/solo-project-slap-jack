0fce91311e4b21b2c43d60e12a51c0931ad7262f
Steve <callasteven@gmail.com>
2021-01-14 08:25:21 -0700

    Create random number generator, randomize deck, deal deck to players

--- Player.js
+++ Player.js
@@ -2,37 +2,49 @@
 var playerOneDeck = [];
 var playerTwoDeck = [];
 
-console.log('1=', playerOneDeck);
-console.log('2=', playerTwoDeck);
+// console.log('1=', playerOneDeck);
+// console.log('2=', playerTwoDeck);
 
-//2) Build Slap Jack deck as deck of 52 cards; start with numbers 1 to 10
+//3) Build Slap Jack deck as deck of 52 cards; start with numbers 1 to 10
 var slapJackDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log('3=', slapJackDeck.length * .5);
+console.log('3=', slapJackDeck);
 
-//2a) Randomize the array
-// for (var i = 0; i < slapJackDeck.length; i++) {
-//   slapJackDeck.push(slapJackDeck[i]);
-//   console.log(slapJackDeck)
-// }
-
-//3) Build for loop to deal cards for player one
-//5) Randomize the deck using #4 below
-//6) Remove player one cards from slapjack deck
-for (var i = 0; i < slapJackDeck.length * 0.5; i++) { //3
-  var x = randomIndex(slapJackDeck.length); //5
-  // console.log('6=', slapJackDeck.length)
-  playerOneDeck.push(slapJackDeck[x]); //5
-  slapJackDeck.splice(x, 1) //6
-  // console.log(slapJackDeck)
-}
-
-//4) Build randomize function
+//3) Create random index generator
 function randomIndex(num) {
-  // console.log('z=', slapJackDeck)
   return Math.floor(Math.random() * num)
 }
 
+//4) Randomize the slapJackDeck array
+function randomizeDeck() {
+  var hold = []; //hold randomly generated cards
+  var index; //hold index to grab random card
+  for (var i = 0; hold.length < slapJackDeck.length; i++) {
+    index =  randomIndex(slapJackDeck.length)
+    if (!hold.includes(index)) {
+      hold.push(index);
+      // console.log('yes')
+      // console.table(i, index, 'yes', hold)
+    }
+  }
+  slapJackDeck = hold;
+  // slapJackDeck.sort()
+  return  slapJackDeck
+}
+// console.log('1=', randomizeDeck());
+
+//3a) Assign initial cards to player #1
+function handForPlayerOne() {
+  var deck = randomizeDeck();
+  for (var i = 0; i < (deck.length* 0.5); i++) {
+    playerOneDeck.push(deck[i]);
+  }
+  for (var i = 0; i < (deck.length* 0.5); i++) {
+    playerTwoDeck.push(deck[i + deck.length * .5]);
+  }
+  console.log('2=', deck, playerOneDeck, playerTwoDeck)
+}
+handForPlayerOne();
 
 //Track Decks
 console.log('1=', playerOneDeck);
