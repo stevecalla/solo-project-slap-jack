e8e6e0c7d041bb134e09a964bd8e7b4d69c5e931
Steve <callasteven@gmail.com>
2021-01-14 21:13:46 -0700

    Add player class

--- Player.js
+++ Player.js
@@ -1,6 +1,17 @@
-// class Player {
-//   constructor() {
-//     this.player = [];
-//     this.id = 'tbd';
-//     this.wins = 0;
-// }
+class Player {
+  constructor(id, deck, wins) {
+    this.id = id;
+    this.player = deck;
+    this.wins = wins || 0;
+    console.table('Player=', this);
+  }
+
+  playCard() {
+    // TBD
+  }
+
+  saveWinsToStorage() {
+    // TBD
+  }
+
+}
--- main.js
+++ main.js
@@ -6,11 +6,27 @@ var listenOnSlapJack = document.querySelector('.title');
 listenOnSlapJack.addEventListener('click', test);
 
 function test() {
-  console.log('a=', startGame);
-  console.log('b=', startGame.playerOneDeck);
-  console.log('c=', startGame.allCards);
-  console.log('d=', startGame.randomIndex(startGame.allCards.length));
-  console.log('e=', startGame.randomizeDeck(startGame.allCards));
+  console.log('x')
+  // console.log('a=', startGame);
+  // console.log('b=', startGame.playerOneDeck);
+  // console.log('c=', startGame.allCards);
+  // console.log('d=', startGame.randomIndex(startGame.allCards.length));
+  // console.log('e=', startGame.randomizeDeck(startGame.allCards));
   console.log('f=', startGame.handForPlayers());
-  console.log('h=', startGame.getDeck());
+  console.log('g=', startGame.playerOneDeck[0].value)
+  // if(startGame.playerOneDeck[0].value === startGame.playerOneDeck[0].value) {
+  //   console.log(true);
+  // } else {
+  //   console.log(false);
+  // }
+  // if(startGame.playerOneDeck[0].value === startGame.playerOneDeck[1].value) {
+  //   console.log(true);
+  // } else {
+  //   console.log(false);
+  // }
+  // if(startGame.playerOneDeck[0] === startGame.playerOneDeck[14]) {
+  //   console.log(true);
+  // } else {
+  //   console.log(false);
+  // }
 }
